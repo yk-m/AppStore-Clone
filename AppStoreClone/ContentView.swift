6b346cd916f1bf2ca7041ef3f4b00eab3497295c
Yuka Matsuo <matsuo.yuka@nifty.co.jp>
2019-10-10 11:44:13 +0900

    first commit

---
+++ AppStoreClone/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  AppStoreClone
+//
+//  Created by sci01725 on 2019/10/10.
+//  Copyright © 2019 Yuka Matsuo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
